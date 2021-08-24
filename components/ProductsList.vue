daa62adbf8a243adb13ee76ba3a85f185f803f0c
Francis Onwumere <fran@mobnia.com>
2021-08-24 16:06:07 +0100

    implement Products List as component

---
+++ components/ProductsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul v-if="productsList.length" class="grid grid-cols-2 gap-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4  py-9">
+    <li v-for="(product, index) in productsList" :key="index" class="col-span-1 flex flex-col text-center bg-red-200">
+      <!--        { "id": 31, "name": "Sharp Cheddar", "category": 8, "image": "http://trayvonnorthern.com/Edgewood-API/public/uploads/5de9bac312193img.jpg", "sizes": 9, "featured": 4, "price": 10, "description": "Smooth, savory, cloth-bound. Aged 6-12 months.", "is_active": 1, "created_at": null, "updated_at": "2020-02-11 18:02:44", "catname": "Cheese", "catimage": null, "catdescription": "Variety of Cheeses", "size": "8 oz" }-->
+      <div class=" relative">
+        <div class=" inset-0  w-full">
+          <img class="w-full max-h-70 object-contain object-center" :src="product.image" :alt="product.name">
+          <div class="flex flex-col flex-1 p-4 mx-auto">
+            <h2 class="text-base tracking-normal text-gray-800 pb-0 mb-0 tk-brandon-grotesque">
+              {{ product.name }}
+            </h2>
+            <p class="text-sm font-normal tracking-normal text-gray-800 tk-brandon-grotesque">
+              {{ product.price }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ProductsList',
+  props: {
+    productsList: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,6 @@
         <div class="flex items-center justify-between w-40 h-10 px-3 border-2 border-gray-300 rounded hover:bg-gray-300">
           <span class="font-medium">
             {{ selectedCategory }}
-            <!--[ { "text": "Dairy", "value": "Dairy", "description": "Our display of cream line milk", "image": null }, { "text": "Cheese", "value": "Cheese", "description": "Variety of Cheeses", "image": null }, { "text": "Specialties", "value": "Specialties", "description": "Seasonal time offers", "image": null } ]-->
           </span>
           <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -36,32 +35,17 @@
         </div>
       </button>
     </div>
-    <ul v-if="products.length" class="grid grid-cols-2 gap-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4  py-9">
-      <li v-for="(product, index) in productsList" :key="index" class="col-span-1 flex flex-col text-center bg-red-200">
-        <!--        { "id": 31, "name": "Sharp Cheddar", "category": 8, "image": "http://trayvonnorthern.com/Edgewood-API/public/uploads/5de9bac312193img.jpg", "sizes": 9, "featured": 4, "price": 10, "description": "Smooth, savory, cloth-bound. Aged 6-12 months.", "is_active": 1, "created_at": null, "updated_at": "2020-02-11 18:02:44", "catname": "Cheese", "catimage": null, "catdescription": "Variety of Cheeses", "size": "8 oz" }-->
-        <div class=" relative">
-          <div class=" inset-0  w-full">
-            <img class="w-full max-h-70 object-contain object-center" :src="product.image" :alt="product.name">
-            <div class="flex flex-col flex-1 p-4 mx-auto">
-              <h2 class="text-base tracking-normal text-gray-800 pb-0 mb-0 tk-brandon-grotesque">
-                {{ product.name }}
-              </h2>
-              <p class="text-sm font-normal tracking-normal text-gray-800 tk-brandon-grotesque">
-                {{ product.price }}
-              </p>
-            </div>
-          </div>
-        </div>
-      </li>
-    </ul>
+    <ProductsList :products-list="productsList" />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import ProductsList from '../components/ProductsList'
 
 export default {
   name: 'DairyShopHome',
+  components: { ProductsList },
   data () {
     return {
       selectedCategory: 'All Products',
